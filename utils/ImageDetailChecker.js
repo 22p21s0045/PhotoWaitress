d98b95d2bd2496fc225305bf84ffd6d238929afc
22p21s0045 <monkeytitle@hotmail.com>
2024-11-26 11:17:20 +0700

    add: model"

--- utils/ImageDetailChecker.js
+++ utils/ImageDetailChecker.js
@@ -1,4 +1,3 @@
-import ollama from "ollama"
 import fs from "fs"
 import appRootPath from "app-root-path"
 export class ImageDetailChecker {
@@ -8,38 +7,5 @@ constructor(imagePath){
 }
 
 
-  async getImageDetail() {
-    const prompt = `
-Provide a JSON response without wrapping it in triple backticks. Include the following structure and possible values:
-
-[Do not wrap the response in backticks or any other formatting.]
-
-
-image_quality:
-Description: Represents the clarity and detail of the image.
-Possible values: "poor", "average", "good", "excellent".
-
-subject_focus:
-Description: Determines how sharply the subject is highlighted in the image.
-Possible values: "low", "medium", "high".
-
-isCloseEye:
-Description: Indicates whether the subject's eyes are closed.
-Possible values: true,false
-`
   
-    const binaryData = fs.readFileSync(this.imagePath)
-    const base64EncodedData = Buffer.from(binaryData).toString("base64")
-    const response = await ollama.chat({
-      model: "llava",
-      messages: [
-        {
-          role: "user",
-          content: prompt,
-          images: [base64EncodedData],
-        },
-      ],
-    })
-    return JSON.parse(response.message.content)
-  }
 }
